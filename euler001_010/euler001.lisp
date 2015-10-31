b51fac38fb2ba375195103560dad0cfe6482b432
Jordy Moos <jordymoos@gmail.com>
2015-10-31 14:09:36 +0100

    Not really sure how to format the code

--- euler001_010/euler001.lisp
+++ euler001_010/euler001.lisp
@@ -1,19 +1,12 @@
 
 
 (defun sum-of-mod (mod max)
-    (*
-        (/
-            (*
-                (floor max mod)
-                (1+ (floor max mod))
-            )
-        2)
-    mod)
-)
+    (* (/ (* (floor max mod)
+             (1+ (floor max mod)))
+          2)
+       mod))
 
 (defun euler1 ()
-    (-
-        (+  (sum-of-mod 3 999)
+    (-  (+  (sum-of-mod 3 999)
             (sum-of-mod 5 999))
-        (sum-of-mod 15 999))
-)
+        (sum-of-mod 15 999)))
